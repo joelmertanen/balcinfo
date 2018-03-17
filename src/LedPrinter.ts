d9b87fa339dab468d2ed0e0759390ec004b29c62
Joel Mertanen <joel.mertanen@gmail.com>
2018-03-18 00:12:07 +0200

    try to show the message

--- src/LedPrinter.ts
+++ src/LedPrinter.ts
@@ -1,7 +1,20 @@
-const senseLed: SenseHatMatrix = require('sense-hat-matrix');
+const senseLed: SenseHatLed = require('sense-hat-led');
 
-const print = () => {
-    
+const print = (text: string) => {
+    const textColor: RGB = [250, 142, 142];
+    const bgColor: RGB = [199, 228, 210];
+
+    return new Promise((resolve, reject) => {
+        const cb: WhenDone = (err) => {
+            if (!err) {
+                resolve();
+            } else {
+                console.error(err);
+                reject(err);
+            }
+        }
+        senseLed.showMessage(text, 0.1, bgColor, textColor, cb);
+    })
 };
 
 export default print;
---
+++ src/types/sense-hat-led.d.ts
@@ -0,0 +1,16 @@
+declare type RGB = Array<number>[3];
+
+declare type FrameBuffer = {};
+declare type LedScreen = Array<RGB>[64];
+declare type WhenDone = (err: string, data: any) => void;
+
+declare interface SenseHatLed {
+    clear: (rgb: RGB) => void,
+    flipH: (redraw: boolean) => LedScreen,
+    flipV: (redraw: boolean) => LedScreen,
+    getPixel: (X: number, Y: number) => RGB,
+    showMessage: (text: string, speed: number, textColour: RGB, backColor: RGB, whenDone: WhenDone) => void,
+    setPixel: (X: number, Y: number, red: number, green: number, blue: number, whenDone: WhenDone) => void,
+    setPixels: (pixels: LedScreen, whenDone: WhenDone) => void,
+    setRotation: (degrees: 0 | 90 | 180 | 270, redraw: boolean, whenDone: WhenDone) => void
+}
