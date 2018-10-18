459052426e1f3cd29a4ffaccebc7e11234fbe22b
Joel Mertanen <joel.mertanen@gmail.com>
2018-10-18 16:42:21 +0300

    small cleanings to make this actually work again

--- src/Persist/ResultsPersisterWeb.ts
+++ src/Persist/ResultsPersisterWeb.ts
@@ -3,25 +3,28 @@ import config from '../../config.json';
 import { PersistedResult} from '../RedisPersister';
 
 const options = {
-    uri: config.uri,
-    method: 'POST',
-    headers: {
-        'x-api-key': config.apiKey
-    },
-    json: true
+  uri: config.uri,
+  method: 'POST',
+  json: true
 };
 
 const sendResults = async (results: Array<PersistedResult>) => {
-    results.forEach(async result => {
-        const payload = {
-            timestamp: (new Date()).toJSON(),
-            temperature: result.temperature,
-            humidity: result.humidity
-        };
+  results.forEach(async result => {
+    const payload = {
+      key: config.apiKey,
+      timestamp: result.timestamp,
+      temperature: result.temperature,
+      humidity: result.humidity
+    };
 
-        const request = Object.assign({}, options, { body: payload });
-        await rp(request);
-    })
+    const request = Object.assign({}, options, { body: payload });
+    try {
+      await rp(request);
+    } catch (e) {
+      console.error('Failed to persist: ', e);
+      throw new Error('failed to persist results');
+    }
+  })
 };
 
 export default sendResults;
